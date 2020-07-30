355ac3463b7432ddcd57716890ef07ace2b3dc9a
paulzay <paulotieno2@gmail.com>
2020-07-30 15:39:34 +0300

    add /william scenario to retrieve his sonnets

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,4 +1,6 @@
 require 'telegram/bot'
+require_relative 'william.rb'
+
 
 class Bot
   def initialize
@@ -8,7 +10,7 @@ class Bot
       bot.listen do |message|
         case message.text.downcase
         when '/start'
-          bot.api.send_message(chat_id: message.chat.id, text: "Hi I am Jarvis. Tony Stark made me just for you. If you're reading this it means he's gone. \n Enter /start to initialize me, \n To shut me down, enter /stop .")
+          bot.api.send_message(chat_id: message.chat.id, text: "Hi I am Jarvis. Tony Stark made me just for you to keep your spirit high. If you're reading this it means he's gone. \n Enter /start to initialize me. To get poems by William Shakespeare, enter \n /william . \n To shut me down, enter /stop .")
 
         when 'Good Morning', 'Good Evening', 'HELLO', 'HI'
           bot.api.send_message(chat_id: message.chat.id, text: "Hi, #{message.from.first_name}, it's a pleasure.")
@@ -16,6 +18,10 @@ class Bot
         when '/stop'
 
           bot.api.send_message(chat_id: message.chat.id, text: "Initializing shut down sequence. See ya later #{message.from.first_name}", date: message.date)
+        when '/william'
+          poems = Shakespeare.new
+          poem = poems.random_poem
+          bot.api.send_message(chat_id: message.chat.id, text: (poem['lines']).to_s, date: message.date)
         end
       end
     end
