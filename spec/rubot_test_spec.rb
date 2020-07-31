46ab7e51d3cfdcd24993911a582d0ea3b96f3be8
paulzay <paulotieno2@gmail.com>
2020-07-31 10:50:43 +0300

    add unit tests for shakespeare class

---
+++ spec/rubot_test_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../lib/william.rb'
+
+describe Shakespeare do
+  describe '#fetch_poems' do
+    let(:request) { Shakespeare.new }
+    let(:response) { request.fetch_poems }
+
+    it 'should return an array of hashes' do
+      expect(response.class).to eql(Array)
+    end
+  end
+
+  describe '#random_poem' do
+    let(:random) { Shakespeare.new }
+    let(:random_response) { random.random_poem }
+
+    it 'should return a hash' do
+      expect(random_response.class).to eql(Hash)
+    end
+
+    it 'should have four key and value pairs' do
+      expect(random_response.length).to eql(4)
+    end
+  end
+end
